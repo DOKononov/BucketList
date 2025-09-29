5e8f1da841589879419dc63865826382b807340d
d.o.kononov <d.o.kononov@gmail.com>
2025-09-29 12:53:01 +0300

    MapKit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,25 +6,39 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var position = MapCameraPosition.region(MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    )
+    
     var body: some View {
-        Button("Read and write") {
-            let data = Data("Test message".utf8)
-            let url = URL.documentsDirectory.appending(path: "message.txt")
-            do {
-                try data.write(to: url, options: [.atomic, .completeFileProtection])
-                let input = try String(contentsOf: url, encoding: .utf8)
-                print(input)
-            } catch {
-                print(error.localizedDescription)
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid)
+                .onMapCameraChange {
+                    print($0.region)
+                }
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
             }
         }
     }
     
-    private func test() {
-        print(URL.desktopDirectory)
-    }
 }
 
 #Preview {
