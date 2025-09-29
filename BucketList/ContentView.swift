0f4a2a0021fcc2908568c43b109f789e9e7ecbd3
d.o.kononov <d.o.kononov@gmail.com>
2025-09-29 11:05:46 +0300

    init

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Read and write") {
+            let data = Data("Test message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url, encoding: .utf8)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
-        .padding()
+    }
+    
+    private func test() {
+        print(URL.desktopDirectory)
     }
 }
 
