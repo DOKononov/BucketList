a96680e811f8d08d179a4104da72190bae794ab9
d.o.kononov <d.o.kononov@gmail.com>
2025-10-04 11:30:41 +0300

    add edite view

---
+++ BucketList/EditeView.swift
@@ -0,0 +1,18 @@
+//
+//  EditeView.swift
+//  BucketList
+//
+//  Created by Dmitry Kononov on 4.10.25.
+//
+
+import SwiftUI
+
+struct EditeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditeView()
+}
